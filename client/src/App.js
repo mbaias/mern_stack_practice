9ad7d8afe6f3e744c1b18a26cf49408bcda7edbf
javier <mametjanov.baias@gmai.com>
2018-08-11 13:55:02 +0600

    added navbar using reactstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import AppNavbar from './components/AppNavbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <AppNavbar />
         <h1>Hello</h1>
       </div>
     );
